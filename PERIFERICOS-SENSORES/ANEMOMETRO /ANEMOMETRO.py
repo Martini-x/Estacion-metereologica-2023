2bd4c1156b2cb7f1a5442ceb9b958f99bfa51da9
Martini-x <110830039+Martini-x@users.noreply.github.com>
2023-10-23 12:51:09 -0300

    Create ANEMOMETRO.py

---
+++ PERIFERICOS-SENSORES/ANEMOMETRO /ANEMOMETRO.py
@@ -0,0 +1,35 @@
+from machine import Pin
+import utime
+import math
+
+# Configura el pin del sensor de efecto Hall
+hall_sensor_pin = Pin(12, Pin.IN)
+
+# Inicializa variables
+last_time = None  # Inicializa la variable para el primer toque
+
+def button_handler(pin):
+    global last_time
+    current_time = utime.ticks_ms()  # Registra la marca de tiempo actual
+    if last_time is not None:
+        interval = (current_time - last_time) / 1000  # Calcula el intervalo entre toques
+        print("Intervalo entre toques (s):", interval)
+
+        # Calcula la velocidad angular en radianes por segundo
+        velocidad_angular_rad_s = 2 * math.pi / interval
+        velocidad_km = (0.023 * velocidad_angular_rad_s) * 3.6
+        print("Velocidad (km/h):", velocidad_km)
+
+    last_time = current_time  # Actualiza la marca de tiempo
+
+try:
+    button_pin = Pin(12, Pin.IN)  # Assuming you have a button connected to Pin 0
+    button_pin.irq(trigger=Pin.IRQ_FALLING, handler=button_handler)
+
+    while True:
+        utime.sleep(1)  # Espera 1 segundo
+
+except KeyboardInterrupt:
+    button_pin.irq(handler=None)  # Detiene la interrupción al salir del programa
+    print("Programa detenido")
+
